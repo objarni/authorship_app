62ae2cfc369efb9460ca767f4b65c9d29e7b3733
Olof Bjarnason <olof.bjarnason@gmail.com>
2017-06-25 20:32:22 +0200

    add medium score class

--- src/components/Author.tsx
+++ src/components/Author.tsx
@@ -2,12 +2,20 @@ import * as React from "react";
 import "./Author.css"
 export interface AuthorProps { email: string; score: number; }
 
+function scoreClass(score: number) {
+	if(score < 40)
+		return '';
+	if(score < 80)
+		return 'is-medium';
+	return 'is-high';
+}
+
 export const Author = ({email, score}: AuthorProps) => 
 	<li className="author">
 		<span className="author-email">
 			{ email  }
 		</span>
-		<span className={`author-score ${score >= 80 ? 'is-high' : ''}`}>
+		<span className={`author-score ${scoreClass(score)}`}>
 			{ score  }
 		</span>
 	</li>;
