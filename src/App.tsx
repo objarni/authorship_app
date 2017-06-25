099727daf3c2b7a07c1ed629ec33ad3cbc0912e3
Olof Bjarnason <olof.bjarnason@gmail.com>
2017-06-25 20:07:13 +0200

    add constructor and compact rendering code

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,31 @@ import { AuthorList } from "./components/AuthorList";
 import { Author } from "./components/Author";
 
 class App extends React.Component<any, any> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            authors:
+            [
+                {
+                    email: "olof.bjarnason@gmail.com",
+                    score: 80.0
+                },
+                {
+                    email: "neppord@gmail.com",
+                    score: 20.0
+                }
+            ]
+        };
+    }
+
     public render() {
+        const {authors} = this.state;
         return (
             <div className="app">
                 <h1>Authorship Visualizer</h1>
                 <h2>Commit: #239087233nmeroi234io</h2>
-                <AuthorList>
-                    <Author email="olof.bjarnason@gmail.com" score={80.0} />
-                    <Author email="neppord@gmail.com" score={20.0} />
-                </AuthorList>
+                <AuthorList>{authors.map((a: any) => <Author {...a} />)}</AuthorList>
             </div>
         );
     }
