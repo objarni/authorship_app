8e1ef6c777623b9c1154fccaca2df71d8ed0f5c9
Samuel Ytterbrink <neppord@gmail.com>
2017-07-01 22:02:08 +0200

    Remove debug code.

--- src/Data.tsx
+++ src/Data.tsx
@@ -1,6 +1,4 @@
 import {processRawData} from "./processRawData"
 import {rawData} from "./RawData"
 
-const processed = processRawData(rawData);
-console.log(processed);
 export const data = processRawData(rawData);
